bc508c3afb364c747581a322b7da36018d2a08c4
Num1ss <krutojmer76@gmail.com>
2023-11-09 19:06:31 +0600

    Выполнил домашнее задание #1

--- hw_1/main.py
+++ hw_1/main.py
@@ -1,30 +1,43 @@
 import asyncio
 import logging
-from os import getenv
 
-from handlers.picture import picture_router
-from handlers.start import start_router
-from handlers.myinfo import myinfo_router
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
 
-from aiogram import Bot, Dispatcher
-from aiogram.enums import ParseMode
+import random
+import os
 
-TOKEN = "6595789384:AAH9Yl6JCvCKw7FBNWqFlypZVJuPpc6CVro"
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token="6595789384:AAH9Yl6JCvCKw7FBNWqFlypZVJuPpc6CVro")
+dp = Dispatcher()
 
-async def main() -> None:
-    # Dispatcher is a root router
-    dp = Dispatcher()
-    # Register all the routers from handlers package
-    dp.include_routers(
-        start_router,
-        picture_router,
-        myinfo_router
-    )
 
-    bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.reply(f"Hello! {message.from_user.first_name}")
 
+@dp.message(Command("myinfo"))
+async def cmd_myinfo(message: types.Message):
+    # print(
+    await message.reply(f"""
+    Ваши данные:
+    ID: {message.from_user.id}
+    Full Name: {message.from_user.full_name}
+    First Name: {message.from_user.first_name}
+    Last Name: {message.from_user.last_name}
+    Username: {message.from_user.username}
+    Telegram Premium: {'Отсутствует' if message.from_user.is_premium is None else 'Присутствует'}
+    """)
+
+@dp.message(Command("picture"))
+async def cmd_picture(message: types.Message):
+    image = random.choice(os.listdir(path='./images'))
+    image = types.FSInputFile(f'./images/{image}')
+    await message.reply_photo(caption=f'Success!', photo=image)
+
+
+async def main():
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
     asyncio.run(main())
